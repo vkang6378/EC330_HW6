ff66c97020dc244bd7ea96c1eaeb3d40769070a9
vkang6378 <91505648+vkang6378@users.noreply.github.com>
2023-04-21 17:32:58 -0700

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -34,15 +34,48 @@ int Trie::search(string key) {
 
 void Trie::remove(string key) {
     TrieNode *current = root;
-    int x = c - 'a';
     for (char c : key) {
+        int x = c - 'a';
         if (current->children[x] == nullptr) {
-            return;
+            return; // The key is not in the trie, so we cannot remove it
         }
         current = current->children[x];
     }
+    
+    if (current->val == 0) {
+        return; // The key is not in the trie, so we cannot remove it
+    }
 
-    for (key : current->children.empty() == false){
-        delete current;
+    // Set the value to 0, indicating that the key has been removed
+    current->val = 0;
+    map_size--;
+
+    // Check if the current node has any children
+    bool has_children = false;
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        if (current->children[i] != nullptr) {
+            has_children = true;
+            break;
+        }
+    }
+
+    // If the current node has no children, remove it and update the tree_size
+    if (!has_children) {
+        TrieNode *parent = root;
+        for (char c : key) {
+            int x = c - 'a';
+            TrieNode *next_node = parent->children[x];
+            if (next_node == current) {
+                parent->children[x] = nullptr;
+                delete current;
+                tree_size--;
+                break;
+            }
+            parent = next_node;
+        }
     }
 }
+
+
+
+
